206562550eb81830568efe2a34bb63ae8b762618
Constantin <constantinmoskvitin@gmail.com>
2023-10-14 14:31:25 +0400

    now it console.logs the notes

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let marked;
+
+import("https://cdn.jsdelivr.net/npm/marked/marked.min.js")
+  .then((module) => {
+    marked = module.marked;
+    // rest of your code...
+  })
+  .catch((error) => console.error("Error importing marked:", error));
+
 async function fetchRepoFiles() {
   const repoOwner = "cmoskvitin";
   const repoName = "moskvit.in";
@@ -5,27 +14,36 @@ async function fetchRepoFiles() {
 
   try {
     const response = await fetch(
-      `https://api.github.com/repos/${repoOwner}/${repoName}/git/trees/${branchName}/notes?recursive=1`
+      `https://api.github.com/repos/${repoOwner}/${repoName}/git/trees/${branchName}?recursive=1`
     );
     const data = await response.json();
 
     const markdownFiles = data.tree.filter((item) => {
-      return item.type === "blob" && item.path.endsWith(".md");
+      return (
+        item.type === "blob" &&
+        item.path.startsWith("notes/") &&
+        item.path.endsWith(".md")
+      );
     });
 
-    const fileNames = markdownFiles.map((file) =>
-      file.path.split("/").pop().replace(".md", "")
-    );
-    const fileUrls = markdownFiles.map(
-      (file) =>
-        `https://github.com/${repoOwner}/${repoName}/blob/${branchName}/notes/${file.path}`
-    );
+    // Map markdownFiles to an array of objects with the desired structure
+    const notes = markdownFiles.map((file) => {
+      const title = file.path.split("/").pop().replace(".md", "");
+      const url = `https://github.com/${repoOwner}/${repoName}/blob/${branchName}/${file.path}`;
+      return { title, url };
+    });
 
-    console.log("Filenames without .md:", fileNames);
-    console.log("URLs to files:", fileUrls);
+    // Create the result object
+    const result = {
+      notesCount: markdownFiles.length,
+      notes,
+    };
+
+    // Return the result object
+    return result;
   } catch (error) {
     console.error("An error occurred:", error);
   }
 }
 
-fetchRepoFiles();
+fetchRepoFiles().then((result) => console.log(result));
